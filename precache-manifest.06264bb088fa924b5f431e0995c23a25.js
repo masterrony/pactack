9bcb627380c60153198b842fb823e593ac3bd131
XueMing Lee <master.rony@yandex.com>
2020-06-30 00:53:33 +0900

    Updates

---
+++ precache-manifest.06264bb088fa924b5f431e0995c23a25.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "0849921db319c9a38819941ebc0b5514",
+    "url": "/pactack/static/media/salads.0849921d.png"
+  },
+  {
+    "revision": "518434ec71954bee2d55",
+    "url": "/pactack/static/css/main.23e4432d.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pactack/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "db93460056279518f90e",
+    "url": "/pactack/static/js/1.db934600.chunk.js"
+  },
+  {
+    "revision": "d8ec2619410968bc3ace",
+    "url": "/pactack/static/js/runtime~main.d8ec2619.js"
+  },
+  {
+    "revision": "286f5b09071e858823da59f1b3f14d2b",
+    "url": "/pactack/static/media/intro.286f5b09.png"
+  },
+  {
+    "revision": "931da55f8b4c176378a310f17b570204",
+    "url": "/pactack/static/media/round.931da55f.png"
+  },
+  {
+    "revision": "a71ad2ae52d3d6171bbb9621f407f549",
+    "url": "/pactack/static/media/background.a71ad2ae.png"
+  },
+  {
+    "revision": "e9a187cae770779c9afdf06e24eba1e1",
+    "url": "/pactack/static/media/cafe.e9a187ca.jpg"
+  },
+  {
+    "revision": "bbe9e49c0c525bc5b345650a1cf3e110",
+    "url": "/pactack/static/media/bar.bbe9e49c.jpg"
+  },
+  {
+    "revision": "deb8562f0fe003faa46e49459682ddfd",
+    "url": "/pactack/static/media/retail.deb8562f.jpg"
+  },
+  {
+    "revision": "a051fe3e9e57a78b7b3996db3f049ece",
+    "url": "/pactack/static/media/background_second.a051fe3e.png"
+  },
+  {
+    "revision": "25544130d2e2f223476136ae4c2694d6",
+    "url": "/pactack/static/media/background_first.25544130.png"
+  },
+  {
+    "revision": "518434ec71954bee2d55",
+    "url": "/pactack/static/js/main.518434ec.chunk.js"
+  },
+  {
+    "revision": "e93ef94cb08ed4e5707ce9b5ba52ab0d",
+    "url": "/pactack/static/media/buddha.e93ef94c.png"
+  },
+  {
+    "revision": "8251a2f90511eb4a4c7c00a1649e6d48",
+    "url": "/pactack/static/media/background.8251a2f9.png"
+  },
+  {
+    "revision": "57f87c60cc22fb5c22a937f82897594a",
+    "url": "/pactack/static/media/background_first.57f87c60.png"
+  },
+  {
+    "revision": "1a74d806fce953f36c1aa4769053f898",
+    "url": "/pactack/static/media/app_store.1a74d806.png"
+  },
+  {
+    "revision": "195e835274807d51bcc2e7b81b3a5cbf",
+    "url": "/pactack/static/media/play_store.195e8352.png"
+  },
+  {
+    "revision": "3c2b5e0c4f5c231b18e9311f6b50d96f",
+    "url": "/pactack/static/media/background_first.3c2b5e0c.png"
+  },
+  {
+    "revision": "a1517c58a0a24eb9fb7bd957b6153563",
+    "url": "/pactack/static/media/background_second.a1517c58.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pactack/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pactack/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pactack/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pactack/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "db93460056279518f90e",
+    "url": "/pactack/static/css/1.4ac4dfce.chunk.css"
+  },
+  {
+    "revision": "a3b6afb465ad5aa73694ce5a10fa552b",
+    "url": "/pactack/index.html"
+  }
+];
